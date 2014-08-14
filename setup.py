fa286a63f4d4d445ce4385c48eed7a5924ffe77e
tumluliu <nudtlliu@gmail.com>
2014-08-15 01:14:06 +0800

    fix naming error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="pymmrouting",
       version="0.1.0",
       description="""Python implementation of multimodal routing engine
                      based on multimodal shortest path algorithm for
-                     postgis python wrapper (pymmspa4pg)""",
+                     postgis library (libmmspa4pg)""",
       author="Lu LIU",
       author_email="nudtlliu@gmail.com",
-      packages=['mmrouting'])
+      packages=['pymmrouting'])
