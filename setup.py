f8d2d6fab1d6cae1aa337b3cab39e8913a82e3ac
tumluliu <nudtlliu@gmail.com>
2014-09-11 01:49:59 +0800

    change version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="pymmrouting",
-      version="0.1.0",
+      version="0.1.1",
       description="""Python implementation of multimodal routing engine
                      based on multimodal shortest path algorithm for
                      postgis library (libmmspa4pg)""",
