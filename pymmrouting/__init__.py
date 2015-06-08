7d17402c726fa3b759809581bf7464b1a3aa8610
tumluliu <nudtlliu@gmail.com>
2015-06-08 15:12:51 +0800

    add logging initialization codes

--- pymmrouting/__init__.py
+++ pymmrouting/__init__.py
@@ -1,11 +1,28 @@
 #__all__ = ['routeplanner', 'inferenceengine', 'routingresult',
 #           'switchcondition', 'datamodel']
 
+__title__ = 'pymmrouting'
+__version__ = '0.2'
+__author__ = 'LIU Lu'
+__license__ = 'The MIT License'
+__copyright__ = 'Copyright 2014 LIU Lu'
+
 __all__ = ['routeplanner', 'inferenceengine', 'routingresult',
            'switchcondition']
 
-from pymmrouting.routeplanner import MultimodalRoutePlanner
-from pymmrouting.inferenceengine import RoutingPlan, RoutingPlanInferer
-from pymmrouting.routingresult import RoutingResult
-from pymmrouting.switchcondition import SwitchCondition
+from .routeplanner import MultimodalRoutePlanner
+from .inferenceengine import RoutingPlan, RoutingPlanInferer
+from .routingresult import RoutingResult
+from .switchcondition import SwitchCondition
 #from pymmrouting.datamodel import MultimodalNetwork
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
