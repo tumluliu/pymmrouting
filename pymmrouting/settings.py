33f7d071f7730951bfa99fa078b4dc14b3821684
tumluliu <nudtlliu@gmail.com>
2015-06-10 17:59:01 +0800

    add config for pgbouncer - an external Postgresql connection pooling service

--- pymmrouting/settings.py
+++ pymmrouting/settings.py
@@ -2,8 +2,16 @@
 """
 
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 # TODO: Identify the config.json file in a good way
 CONFIG_FILE = "config.json"
 with (open(CONFIG_FILE, 'r')) as conf_file:
-    PG_DB_CONF = json.load(conf_file)["pg_datasource"]["connection"]
+    conf = json.load(conf_file)
+    logger.debug("Get config from %s: %s", CONFIG_FILE, conf)
+    PG_DB_CONF = conf["pg_datasource"]["connection"]
+    logger.debug("Content of ['pg_datasource']['connection'] section: %s", PG_DB_CONF)
+    PGBOUNCER_CONF = conf["pg_datasource"]["pgbouncer"]
+    logger.debug("Content of ['pg_datasource']['pgbouncer'] section: %s", PGBOUNCER_CONF)
